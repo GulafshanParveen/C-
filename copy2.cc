fe0265889a8ac3ce943043be9487a90a1cfe893f
Gulafshan Parveen <64768393+GulafshanParveen@users.noreply.github.com>
2020-06-05 09:52:50 +0530

    Copy constructor & Destructor

--- copy2.cc
+++ copy2.cc
@@ -4,17 +4,26 @@ class Myclass{
 	public:
 	int age,salary;
 	Myclass():age(10),salary(20){}
-	private:
+	Myclass(int a,int s){
+		age=a;
+		salary=s;
+	}
+	~Myclass(){
+		cout<<"Destructor called"<<endl;
+	}
+//	private:
 	Myclass(const Myclass& my):age(my.age),salary(my.salary){}
-	friend int main();
+//	friend int main();
 };
 int main(){
 	Myclass m;
 	cout<<m.age<<" "<<m.salary<<endl;
 	m.age=20;
 	m.salary=2000;
+	Myclass* m1=new Myclass(30,3000);
 	//copy format
-	Myclass m1=m;
-	cout<<m1.age<<" "<<m1.salary<<endl;
+	//Myclass m1=m;
+	cout<<m1->age<<" "<<m1->salary<<endl;
+	delete m1;
 return 0;
 }
